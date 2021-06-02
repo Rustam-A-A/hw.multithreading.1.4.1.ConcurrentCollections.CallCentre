d08f4335c38f33c0ef65264a72de0bd0d0144c10
Rustam Aktuganov <aktuganov@yandex.ru>
2021-06-02 12:01:20 +0200

    with using join() to prevent loosing operator-threads

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         final int TIME_TO_START = 1000;
         final int TIME_TO_STOP_RECEIVING = 10000;
 
@@ -10,19 +10,22 @@ public class Main {
         Thread incomingCalls = new Thread(null, incomingCall::call ,"Входящие звонки");
         incomingCalls.start();
 
+        incomingCalls.join();
 
         //Проверка наличия входящих звонков и выделение необходимого времени
         // для начального формирования очереди
-        while (incomingCall.currentCalls.isEmpty()) {
-            System.out.println("пока звонков нет");
-            try {
-                Thread.sleep(TIME_TO_START);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
+//        while (incomingCall.currentCalls.isEmpty()) {
+//            System.out.println("пока звонков нет");
+//            try {
+//                Thread.sleep(TIME_TO_START);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }
 
-        new Thread(null, operator::takeCall, "Anton").start();
+        Thread operator1 = new Thread(null, operator::takeCall, "Anton");
+//        operator1.join();
+//        operator1.start();
         new Thread(null, operator::takeCall, "Ivan").start();
         new Thread(null, operator::takeCall, "Olga").start();
         new Thread(null, operator::takeCall, "Helmut").start();
